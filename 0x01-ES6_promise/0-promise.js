29983d7645ed2ca52a4d4b88f8cc4235eae05970
jojobutercup <jojobutercup@gmail.com>
2023-04-21 18:50:24 -0700

    commit

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Make API call here
+    // On success, call resolve() with the response
+    // On failure, call reject() with the error
+  });
+}
