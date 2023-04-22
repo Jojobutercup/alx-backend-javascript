e30fe5634ae770c966fd09d8a71173702e8b37f5
Osariemen Uwuilehue <106764970+Jojobutercup@users.noreply.github.com>
2023-04-21 18:57:33 -0700

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,12 @@
-function getResponseFromAPI() {
+export default function getResponseFromAPI() {
+  const success = true;
+
+  // Returns a new Promise with a callback function that takes in a resolve and reject function
   return new Promise((resolve, reject) => {
-    // Make API call here
-    // On success, call resolve() with the response
-    // On failure, call reject() with the error
+    if (success) {
+      resolve(); // If success is true, the Promise is resolved with no value
+    } else {
+      reject(); // If success is false, the Promise is rejected with no value
+    }
   });
 }
