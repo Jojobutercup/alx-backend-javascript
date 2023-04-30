7574c1e13154825f98fc23fda0129e76fc13fc84
jojobutercup <jojobutercup@gmail.com>
2023-04-30 20:52:58 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,22 +1,5 @@
-// 1-get_list_student_ids.js
-
-// Import the getListStudents function from previous task
-import getListStudents from './0-get_list_students';
-
-// Define the getListStudentIds function
-const getListStudentIds = (students) => {
-  // If the argument is not an array, return an empty array
-  if (!Array.isArray(students)) {
-    return [];
-  }
-
-  // Call getListStudents to use it in this file
-  const studentList = getListStudents();
-
-  // Use the map function to extract the ids from the list of objects
-  return students.map((student) => student.id);
-};
-
-// Export the getListStudentIds function
-export default getListStudentIds;
-
+// eslint-disable-next-line consistent-return
+export default function getListStudentIds(arg) {
+  if (!Array.isArray(arg)) return [];
+  return arg.map((el) => el.id);
+}
